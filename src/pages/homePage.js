484c74089912d2271bf75cf5878acb27cf030fce
Leighton Miguel <leim56@hawaii.rr.com>
2024-01-02 00:59:11 -1000

    Update homePage.js

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -9,14 +9,9 @@ export const HomePage = () =>{
     <>
         <Navbar/>
         <div className='container py-5 my-5'>
-            <br></br>
+    
           This is the home page
-          Sincerely, Jalen lum
-          <br>
-          </br>
-
-          Home page is finished
-          - Jalen 1/2/24
+         
         </div>
 
     </>

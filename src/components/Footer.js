4bca351c25222fd5559bbc239e5782f402dc03d1
jalenlum <808jalen@gmail.com>
2024-01-02 22:50:42 -1000

    feet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,129 +1,103 @@
 import React, { useState,useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-export const Footer = ()=>{
+export const Footer = () => {
 
     return(
         <>
-            <div class="container my-5">
+            <footer className="text-center text-lg-start text-white bg-dark">
+                <section className="d-flex justify-content-between p-4" style={{backgroundColor: '#2C4230'}}>
+                    <div className="me-5">
+                        <span>Get connected with us on social networks:</span>
+                    </div>
+                    <div>
+                        <a href="" className="text-white me-4">
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="" className="text-white me-4">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="" className="text-white me-4">
+                            <i className="fab fa-google"></i>
+                        </a>
+                        <a href="" className="text-white me-4">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                        <a href="" className="text-white me-4">
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a href="" className="text-white me-4">
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </div>
+                </section>
 
-                <footer
-                        class="text-center text-lg-start text-white"
-                        // style="background-color: #1c2331"
-                        >
-                    <section
-                            class="d-flex justify-content-between p-4"
-                            // style="background-color: #6351ce"
-                            >
-                        <div class="me-5">
-                            <span>Get connected with us on social networks:</span>
-                        </div>
+                <section className="">
+                    <div className="container text-center text-md-start mt-5">
+                        <div className="row mt-3">
+                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                <h6 className="text-uppercase fw-bold">Company name</h6>
+                                <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{width: 60, height: 2, backgroundColor: '#7c4dff'}}/>
+                                <p>
+                                    Here you can use rows and columns to organize your footer
+                                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                    elit.
+                                </p>
+                            </div>
 
-                        <div>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-google"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-linkedin"></i>
-                            </a>
-                            <a href="" class="text-white me-4">
-                                <i class="fab fa-github"></i>
-                            </a>
-                        </div>
-                    </section>
+                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <h6 className="text-uppercase fw-bold">Products</h6>
+                                <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{width: 60, height: 2, backgroundColor: '#7c4dff'}}/>
+                                <p>
+                                    <a href="#!" className="text-white">MDBootstrap</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-white">MDWordPress</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-white">BrandFlow</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-white">Bootstrap Angular</a>
+                                </p>
+                            </div>
 
-                    <section class="">
-                        <div class="container text-center text-md-start mt-5">
-                            <div class="row mt-3">
-                                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                                    <h6 class="text-uppercase fw-bold">Company name</h6>
-                                    <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
-                                        // style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                    <p>
-                                        Here you can use rows and columns to organize your footer
-                                        content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                                        elit.
-                                    </p>
-                                </div>
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <h6 className="text-uppercase fw-bold">Useful links</h6>
+                                <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{width: 60, height: 2, backgroundColor: '#7c4dff'}}/>
+                                <p>
+                                    <a href="#!" className="text-white">Your Account</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-white">Become an Affiliate</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-white">Shipping Rates</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-white">Help</a>
+                                </p>
+                            </div>
 
-                                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                                    <h6 class="text-uppercase fw-bold">Products</h6>
-                                    <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
-                                        // style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                    <p>
-                                        <a href="#!" class="text-white">MDBootstrap</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-white">MDWordPress</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-white">BrandFlow</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-white">Bootstrap Angular</a>
-                                    </p>
-                                </div>
-
-                                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                                    <h6 class="text-uppercase fw-bold">Useful links</h6>
-                                    <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
-                                        // style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                    <p>
-                                        <a href="#!" class="text-white">Your Account</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-white">Become an Affiliate</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-white">Shipping Rates</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-white">Help</a>
-                                    </p>
-                                </div>
-
-                                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                                    <h6 class="text-uppercase fw-bold">Contact</h6>
-                                    <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
-                                        // style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                    <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                                </div>
+                            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                                <h6 className="text-uppercase fw-bold">Contact</h6>
+                                <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{width: 60, height: 2, backgroundColor: '#7c4dff'}}/>
+                                <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                                <p><i className="fas fa-envelope mr-3"></i> info@example.com</p>
+                                <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                                <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
                             </div>
                         </div>
-                    </section>
-
-                    <div
-                        class="text-center p-3"
-                        // style="background-color: rgba(0, 0, 0, 0.2)"
-                        >
-                        © 2020 Copyright:
-                        <a class="text-white" href="https://mdbootstrap.com/"
-                        >MDBootstrap.com</a
-                        >
                     </div>
-                </footer>
-            </div>
+                </section>
+
+                <div className="text-center p-3" style={{backgroundColor: '#2C4230'}}>
+                    © 2020 Copyright:
+                    <a className="text-white" href="https://mdbootstrap.com/"
+                    >MDBootstrap.com</a
+                    >
+                </div>
+            </footer>
         </>
     )
-
 }

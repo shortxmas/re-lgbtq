afd5115cae4a1c4b5b28c503685000ce9705f403
Leighton Miguel <leim56@hawaii.rr.com>
2024-01-17 20:11:39 -1000

    Update homePage.js

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -51,11 +51,14 @@ export const HomePage = () =>{
 
 
 
-            <div className='text-center my-4 'style={{fontFamily:'Poppins'}} data-sal="slide-down" data-sal-duration="1200">
+            <div className='text-left my-4 'style={{fontFamily:'Poppins'}} data-sal="slide-down" data-sal-duration="1200">
                 <p>We are open to serve you through walk-in counter service and in person or Zoom appointments.</p>
                 <p>Our center is open to provide you with assistance via Zoom, in-person, or over the phone, Monday-Friday from 9:00 am-5:00 pm.</p>
                 <p>Our staff is available to provide resources, referrals and student support during regular business hours. To speak with Director Camaron Miyamoto, please make an appointment with this link: calendly.com/uhmlgbtqcalendar</p>
+                
                 <p>The LGBTQ+ Center Lounge is open during regular business hours. Reservations are not required.</p>
+                <h3>PURPOSE</h3>
+                <p>Lesbian, Gay, Bisexual, Transgender & Queer+ (LGBTQ+) Center works to eliminate systemic barriers to academic excellence and student success, strives to maintain a safe and inclusive campus environment that is free from harassment and discrimination, an seeks to affirm LGBTQ+ and māhū people across our campus community. Our purpose is to empower students to create healthy living-learning communities, enhance interpersonal growth, and celebrate academic achievement. We offer daily direct services, support and referrals via Zoom appointment and on the phone, M-F, 9:00am-5:00pm.</p>
             </div>
 
         </div>

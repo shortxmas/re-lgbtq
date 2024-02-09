cc54d7a742ec917263171ce77862ad819dad1868
Leighton Miguel <leim56@hawaii.rr.com>
2024-02-08 14:30:19 -1000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ export const Navbar = ()=>{
         <   link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet"></link>
 
             <div className="navbar fixed-top navbar-expand-xl navbar-dark" style={{backgroundColor:'#2C4230'}}>
-                <div className="container-fluid">
+                <div className="container">
                     <a href="/lgbtq/" className="navbar-brand">
                      <img src="../../assets/logowhite.png" width="94" height="65" className="d-inline-block align-top" alt=""></img>
                     </a>

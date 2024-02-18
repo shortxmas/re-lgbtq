0c62eb0a389a43b1fda3472b2b23db725b6ca6ea
Leighton Miguel <leim56@hawaii.rr.com>
2024-02-17 23:25:42 -1000

    contact page dynamic render

--- src/components/ContactPageMembers.js
+++ src/components/ContactPageMembers.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { rootUrl } from "../urls";
+import { LoadingImage } from "./LoadingImage";
 
 export const ContactPageMembers = () => {
-
   const [membersData, setMembersData] = useState();
   const [loading, setLoading] = useState(true);
+
+  const [loadingVisibility, changeLoadingVisibility] = useState({
+    alignItems: "center",
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,32 +31,72 @@ export const ContactPageMembers = () => {
     fetchData();
   }, []);
 
+  const renderMembers = () => {
+    const ret = [];
+    membersData.forEach((member) => {
+      if (member.image != null) {
+        ret.push(
+          <div
+            key={member}
+            className="row d-flex justify-content-center align-middle my-2"
+          >
+            <div
+              className="col-12 col-lg-4 d-flex py-2 justify-content-center px-5"
+              style={{}}
+            >
+              <LoadingImage uri={member.image} />
+            </div>
 
-  console.log(membersData)
-
-
-  return (
-    <>
-      <div className="row d-flex justify-content-center align-middle my-2">
-        <div className="col-12 col-lg-4 d-flex py-2 px-5" style={{}}>
-          <img
-            src="../../assets/cam.png"
-            className="img-fluid border"
-            alt="..."
-          ></img>
-        </div>
-
-        <div
-          className="col-12 col-lg-4 d-flex py-2 justify-content-center"
-          style={{ alignItems: "center" }}
-        >
-          <div className="text-start">
-            <h3> Camoran Miyamoto</h3>
-            <h5>Director</h5>
-            <p>lgbtq@hawaii.edu</p>
+            <div
+              className="col-12 col-lg-4 d-flex py-2 px-5"
+              style={{ alignItems: "center" }}
+            >
+              <div style={{ width: "100%" }}>
+                <h3 className="text-start">{member.name}</h3>
+                <h5 className="text-start">{member.title}</h5>
+                <h6 className="text-start">{member.pronouns}</h6>
+                <p className="text-start">{member.email}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </>
+        );
+      } else {
+        ret.push(
+          <div
+            key={member}
+            className="row d-flex justify-content-center align-middle my-2"
+          >
+            <div
+              className="col-12 col-lg-4 d-flex py-2 justify-content-center px-5"
+              style={{}}
+            ></div>
+
+            <div
+              className="col-12 col-lg-4 d-flex py-2 px-5"
+              style={{ alignItems: "center" }}
+            >
+              <div style={{ width: "100%" }}>
+                <h3 className="text-start">{member.name}</h3>
+                <h5 className="text-start">{member.title}</h5>
+                <h6 className="text-start">{member.pronouns}</h6>
+                <p className="text-start">{member.email}</p>
+              </div>
+            </div>
+          </div>
+        );
+      }
+    });
+
+    return ret;
+  };
+
+  console.log(membersData);
+
+  return loading ? (
+    <div className="d-flex justify-content-center " style={loadingVisibility}>
+      <div className="spinner-border my-5" style={loadingVisibility}></div>
+    </div>
+  ) : (
+    <>{renderMembers()}</>
   );
 };
---
+++ src/components/LoadingImage.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+export const LoadingImage = (props) => {
+  const [imageStyle, changeImageStyle] = useState({ display: "none" });
+  const [loadingStyle, changeLoadingStyle] = useState();
+
+  return (
+    <>
+      <div className="d-flex justify-content-center " style={loadingStyle}>
+        <div className="spinner-border my-5" style={loadingStyle}></div>
+      </div>
+
+      <img
+        style={imageStyle}
+        src={props.uri}
+        className="img-fluid border"
+        alt="..."
+        onLoad={() => {
+          changeImageStyle();
+          changeLoadingStyle({ display: "none" });
+        }}
+      ></img>
+    </>
+  );
+};
